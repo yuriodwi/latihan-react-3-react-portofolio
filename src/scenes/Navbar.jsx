08d7e0dc8137a9118f6fa3a3431c7ff7e11973e4
Yurio Dwi LS <rea.riera@gmail.com>
2023-09-26 20:42:01 +0700

    navbar

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -60,6 +60,46 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
             <img src="../assets/menu-icon.svg" alt="menu-icon" />
           </button>
         )}
+
+        {/* MOBILE MENU POPUP */}
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img src="../assets/close-icon.svg" alt="close-icon" />
+              </button>
+            </div>
+            {/* MENU ITEM */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
