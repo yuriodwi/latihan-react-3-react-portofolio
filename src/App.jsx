f08f1167f3a7ffc16fd806cc5ebf1c80aa4b0b87
Yurio Dwi LS <rea.riera@gmail.com>
2023-09-28 21:20:39 +0700

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 const App = () => {
@@ -30,6 +31,7 @@ const App = () => {
             setSelectedPage={setSelectedPage}
           />
         )}
+        <Landing setSelectedPage={setSelectedPage} />
       </div>
     </div>
   );
